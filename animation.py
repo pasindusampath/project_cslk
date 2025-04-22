a5c5f5b3dcb8dd091698127b7226712d1168880c
nithadya <mihisaranithadya148i@gmail.com>
2025-04-22 09:47:13 -0700

    removed comments

--- animation.py
+++ animation.py
@@ -1,18 +1,18 @@
 import turtle
 import time
 
-# Set up the screen
+
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("Simple Animation")
 
-# Create a turtle
+
 anim_turtle = turtle.Turtle()
 anim_turtle.shape("turtle")
 anim_turtle.color("cyan")
 anim_turtle.speed(0)
 
-# Animation loop
+
 for i in range(36):
     anim_turtle.forward(100)
     anim_turtle.right(60)
@@ -24,6 +24,6 @@ for i in range(36):
     anim_turtle.right(100)
     anim_turtle.right(10)
 
-# Pause before closing
+
 time.sleep(3)
 screen.bye()
