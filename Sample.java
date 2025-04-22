629f10603da5a968676f3324174243cff206dc7d
Chamod Anjana <chamodanjana9651@gmail.com>
2025-04-22 22:14:51 +0530

    Added branch - ChamodAnjana

--- Sample.java
+++ Sample.java
@@ -10,6 +10,26 @@ class Cat implements Animal{
 class Bird implements Animal{
 	@Override
 	public void move(){
-		System.out.printl("Bird is Flying");
+		System.out.println("Bird is Flying");
 	}
+}
+
+class Vehicle{
+    public void start(){
+        System.out.println("Vehicle is starting");
+    }
+}
+class Car extends Vehicle{
+    @Override
+    public void start(){
+        System.out.println("Car is starting");
+    }
+}
+public class OverrideDemo{
+    public static void main(String[] args){
+        Vehicle vehicle = new Vehicle();
+        vehicle.start();
+        Vehicle car = new Car();
+        car.start();
+    }
 }
