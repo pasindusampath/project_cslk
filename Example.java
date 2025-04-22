fb14abb536a5706eab8251b76b366952537a2844
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2025-04-22 22:09:06 +0530

    Hello

--- Example.java
+++ Example.java
@@ -3,5 +3,7 @@ class Example{
 		System.out.println("Hello World");
 		System.out.println("Hello World 2");
 		System.out.println("Hello World 3");
+		System.out.println("Hello I am a Java Developer");
+		System.out.println("welcome to the world of Java");
 	}
 }
--- Sample.java
+++ Sample.java
@@ -10,6 +10,23 @@ class Cat implements Animal{
 class Bird implements Animal{
 	@Override
 	public void move(){
-		System.out.printl("Bird is Flying");
+		System.out.println("Bird is Flying");
+	}
+}
+
+class Dog implements Animal{
+	@Override
+	public void move(){
+		System.out.println("Dog is Running");
+	}
+}
+class Sample{
+	public static void main(String[] args){
+		Animal a = new Cat();
+		a.move();
+		a = new Bird();
+		a.move();
+		a = new Dog();
+		a.move();
 	}
 }
