bbd0c100f556ccbd047778816067a8bc094f71d3
SadeepaNHerath <sadeepahearth@gmail.com>
2025-04-22 22:55:02 +0530

    feat: add JavaScript and TypeScript implementations of Example.java with message printing functions

---
+++ example.js
@@ -0,0 +1,16 @@
+// JavaScript implementation of Example.java
+
+function printMessages() {
+  // Print messages with proper formatting
+  console.log("===== JavaScript Demo Program =====");
+  console.log("Line 1: Hello World");
+  console.log("Line 2: Hello JavaScript Programming");
+  console.log("Line 3: Learning JavaScript Fundamentals");
+  console.log("============================");
+  console.log("Line 4: JavaScript is a versatile language");
+  console.log("Hello I am a JavaScript Developer");
+  console.log("welcome to the world of JavaScript");
+}
+
+// Call the function when the script is executed
+printMessages();
