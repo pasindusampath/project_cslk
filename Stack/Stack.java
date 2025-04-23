fd6459df7e160015610d88b8013984f7dde9230b
lk-bl <contact@lkbl.dev>
2025-04-23 21:31:33 +0530

    refact: better practices
    refactors the Stack class to improve code readability, consistency, and performance

--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,48 +1,45 @@
-package Stack;
-
+package stack;
 
 public class Stack {
-	
-	private int top;
-	private int maxSize;
-	private char[] stackArray;
-	
-	public Stack(int maxSize) {
-		this.maxSize = maxSize;
-		this.top = -1;
-		this.stackArray = new char [maxSize];
-	}
-	
-	public boolean isFull() {
-		return top == maxSize - 1;
-	}
-	
-	public boolean isEmpty() {
-		return  top == -1;
-	}
-	
-	public void push(char value) {
-		if(isFull()) {
-			System.out.println("Stack is Full");
-		}else {
-			stackArray[++top] = value;
-		}
-	}
-	
-	public char pop() {
-		if(isEmpty()) {
-			System.out.println("Stack is Empty");
-			return '\0';
-		}
-		return stackArray[top--];
-		}
-	
-	public char peek() {
-		if(isEmpty()) {
-			System.out.println("Stack is Empty");
-			return '\0';
-		}
-		return stackArray[top];
-		}
+    private int top;
+    private final int capacity;
+    private final char[] stackArray;
 
+    public Stack(int capacity) {
+        this.capacity = capacity;
+        this.top = -1;
+        this.stackArray = new char[capacity];
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public void push(char value) {
+        if (isFull()) {
+            System.out.println("Stack is Full");
+            return;
+        }
+        stackArray[++top] = value;
+    }
+
+    public char pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return '\0';
+        }
+        return stackArray[top--];
+    }
+
+    public char peek() {
+        if (isEmpty()) {
+            System.out.println("Stack is Empty");
+            return '\0';
+        }
+        return stackArray[top];
+    }
 }
