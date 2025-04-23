87e816dbf46cc7ef120e1b34fb07e7defe8bcccf
lk-bl <contact@lkbl.dev>
2025-04-23 21:28:54 +0530

    refact; feat: Better practices

--- Stack/Main.java
+++ Stack/Main.java
@@ -3,36 +3,40 @@ package Stack;
 import java.util.Scanner;
 
 public class Main {
-	
-	static int maxSize = 10;
 
-	public static void main(String[] args) {
-		
-		Queue queue = new Queue(maxSize);
-		Stack stack = new Stack(maxSize);
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println(" Insert 5 characters in acsending order: ");
-		for(int i=0; i < 5; i++) {
-			char value = scanner.next().charAt(0);
-			queue.insert(value);
-			stack.push(value);
-			
-		}
-		
-		while(!stack.isEmpty()) {
-			queue.insert(stack.pop());
-		}
-		
-		System.out.println("Final list elements of queue: ");
-		while(!queue.isEmpty()) {
-			System.out.println(queue.delete());
-			
-		}
-		scanner.close();
-	
-	}
+    private static final int MAX_SIZE = 10;
 
+    public static void main(String[] args) {
+        Queue queue = new Queue(MAX_SIZE);
+        Stack stack = new Stack(MAX_SIZE);
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            collectInput(scanner, queue, stack);
+            mirrorStackToQueue(stack, queue);
+            displayFinalQueue(queue);
+        }
+    }
+
+    private static void collectInput(Scanner scanner, Queue queue, Stack stack) {
+        System.out.println("Insert 5 characters in ascending order:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter character " + (i + 1) + ": ");
+            char value = scanner.next().charAt(0);
+            queue.insert(value);
+            stack.push(value);
+        }
+    }
+
+    private static void mirrorStackToQueue(Stack stack, Queue queue) {
+        while (!stack.isEmpty()) {
+            queue.insert(stack.pop());
+        }
+    }
+
+    private static void displayFinalQueue(Queue queue) {
+        System.out.println("Final list elements of the queue:");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.delete());
+        }
+    }
 }
-
