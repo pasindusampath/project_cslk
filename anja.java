da23c59fed17ece7c053649105548ce42deb1352
Anjana Dilshan <dilaking97@gmail.com>
2025-04-24 00:30:00 +0530

    initial commit

---
+++ anja.java
@@ -0,0 +1,25 @@
+import java.util.*;  //Bubble Sort...............2025/04/23 (jp 27)
+
+public class anja {
+	
+	public static void main (String[] args) {
+		int [] ar = {10,15,35,90,4,7,36,80};
+		System.out.println("BEfore Short :"+Arrays.toString(ar));
+		
+		for (int j = 0; j < ar.length; j++)
+		{
+			for (int i = 0; i < ar.length - 1; i++) {
+			if (ar[i] < ar[i + 1]) { 
+				int temp = ar[i + 1];
+				ar[i + 1] = ar[i];
+				ar[i] = temp;
+			}
+		}
+	       System.out.println("After   Short "+j+":" + Arrays.toString(ar));
+		}
+		
+		
+	
+	
+	}
+}
