c9f16d1bdc648998ef436131372250966a7c53db
lk-bl <contact@lkbl.dev>
2025-04-23 21:30:31 +0530

    refact: better practices
    refactors the existing Queue implementation for improved readability, maintainability, and efficiency

--- Stack/Queue.java
+++ Stack/Queue.java
@@ -1,70 +1,57 @@
-package Stack;
-
+package stack;
 
 public class Queue {
-	private int rear;
-	private int front;
-	private int no_of_items;
-	private int maxSize;
-	private char[] queueArray;
-	
-	
-	public Queue(int maxSize) {		
-		this.maxSize = maxSize;
-		this.rear = -1;
-		this.front = 0;
-		this.no_of_items = 0;
-		this.queueArray = new char [maxSize];
-	}
-	
-	public boolean isEmpty() {
-		return no_of_items == 0;
-	}
-	
-	public boolean isFull() {
-		return no_of_items == maxSize;
-		
-	}
-	
-	public void insert(char value) {
-		if (isFull()) {
-			System.out.println("Queue is Full");
-		}else {
-			if(rear == maxSize - 1) {
-				rear = - 1;
-			}
-			queueArray[++rear] = value;
-			no_of_items++;
-		}
-	}
-	
-	public char delete() {
-		
-		char temp = queueArray[front++];
-		
-		if (isEmpty()) {
-			System.out.println("Queue is Empty");
-			return '\0';
-		}else {
-			if(front == maxSize) {
-				front = 0;				
-			}
-			
-			no_of_items--;
-			return temp;
-		}
-	}
-	
-	public char peekFront() {		
-		
-		if (isEmpty()) {
-			System.out.println("Queue is Empty");
-			return '\0';
-		}else {			
-			return queueArray[front];
-		}
-	}
-	
-	
+    private int rear;
+    private int front;
+    private int size;
+    private final int capacity;
+    private final char[] queueArray;
 
+    public Queue(int capacity) {
+        this.capacity = capacity;
+        this.rear = -1;
+        this.front = 0;
+        this.size = 0;
+        this.queueArray = new char[capacity];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    public void insert(char value) {
+        if (isFull()) {
+            System.out.println("Queue is Full");
+            return;
+        }
+
+        rear = (rear + 1) % capacity;
+        queueArray[rear] = value;
+        size++;
+    }
+
+    public char delete() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            return '\0';
+        }
+
+        char value = queueArray[front];
+        front = (front + 1) % capacity;
+        size--;
+        return value;
+    }
+
+    public char peekFront() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+            return '\0';
+        }
+
+        return queueArray[front];
+    }
 }
