da87d61b43c877607eff4b953456237b64013d39
Ramitha Heshan <142712365+RamithaHeshan33@users.noreply.github.com>
2025-04-24 09:40:11 +0530

    day3 (#33)

---
+++ Java_Class/Day3/Example.java
@@ -0,0 +1,23 @@
+
+public class Example {
+    // public static void main(String[] args) {
+    // int decimalNumber = 100;
+    // System.out.println("Decimal: " + decimalNumber);
+    // }
+
+    // public static void main(String[] args) {
+    // int octalNumber = 0144; // Equivalent to 100 in decimal
+    // System.out.println("Octal: " + octalNumber);
+    // }
+
+    public static void main(String[] args) {
+        int binaryNumber = 0b1100100; // Equivalent to 100 in decimal
+        System.out.println("Binary: " + binaryNumber);
+    }
+
+    // public static void main(String[] args) {
+    // int hexNumber = 0x64; // Equivalent to 100 in decimal
+    // System.out.println("Hexadecimal: " + hexNumber);
+    // }
+
+}
