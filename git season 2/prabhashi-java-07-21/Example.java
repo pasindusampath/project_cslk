aa54c077c13ee6430424ff888018ccc9aa436960
DUMMY <contact@lkbl.dev>
2025-04-25 21:57:01 +0530

    refact: Example.java

--- git season 2/prabhashi-java-07-21/Example.java
+++ git season 2/prabhashi-java-07-21/Example.java
@@ -1,5 +1,5 @@
 class Example {
-	puclic static void main(String args[]){
-		System.out.println("Hello everyone");
-	}
+    puclic static void main(String[] args){
+        System.out.println("Hello everyone");
+    }
 }
