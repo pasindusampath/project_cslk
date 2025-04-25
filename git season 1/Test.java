51e9a79bbfba502fd60d55eceb60323103ddc792
pasindusampath <pasindub32@gmail.com>
2025-04-25 17:21:59 +0530

    minor changes

--- git season 1/Test.java
+++ git season 1/Test.java
@@ -1,18 +1,12 @@
 import java.util.Scanner;
 
-
-public class test {
-
+public class Test {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
-
         System.err.println("Enter the number of elements in the array: ");
         int n = sc.nextInt();
-
         int arr[] = new int[n];
         System.err.println("Enter the elements of the array: ");
-
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
             System.err.println("Element " + i + ": " + arr[i]);
@@ -21,10 +15,5 @@ public class test {
         for (int i = 0; i < n; i++) {
             System.err.println(arr[i]);
         }
-       
-
-
-
-    }
-    
+    }  
 }
