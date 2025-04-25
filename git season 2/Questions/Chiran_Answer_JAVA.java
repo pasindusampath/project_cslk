2c5b5baa3cee33d408e56e56151a4f698c441a0c
Chiranjeewa Peellawatta <chiranjeewalankeshwara@gmail.com>
2025-04-25 22:50:15 +0530

    Add initial implementation of Chiran_Answer_JAVA class with main method

---
+++ git season 2/Questions/Chiran_Answer_JAVA.java
@@ -0,0 +1,6 @@
+public class Chiran_Answer_JAVA {
+    public static void main(String[] args) {
+        String name = "Chiranjeewa Lankeshwara";
+        System.out.println("My Name: " + name);
+    }
+}
