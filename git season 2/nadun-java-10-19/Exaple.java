ea9f3a3ab34f01d4bbdce9360295512dfd69d530
Nadun Randeera <126862247+Nadunrandeera@users.noreply.github.com>
2025-04-25 21:50:43 +0530

    wrote my first java code

---
+++ git season 2/nadun-java-10-19/Exaple.java
@@ -0,0 +1,11 @@
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+
+        System.out.println("Sum: " + sum);
+    }
+}
